56d1503243e558f43471882a9b30d35b8eb1f2a6
Klamichhane738.Keshav <lami0053@algonquinlive.com>
2025-03-06 16:15:05 -0500

    Fix Terraform formatting

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
@@ -5,7 +16,7 @@ provider "azurerm" {
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "East US"
-  }
+}
 
 resource "azurerm_storage_account" "example" {
   name                     = "examplestoracc"
